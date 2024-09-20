b0fd1b2a3a7ddad5d8f76adace855641110a48a0
Sondes <sondes.oueslati@lovotech.fr>
2024-09-20 14:31:49 +0200

    feature-creation_courses_manager_app

---
+++ src/main/java/com/pmu/coursesmanager/CoursesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pmu.coursesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManagerApplication.class, args);
+	}
+
+}
